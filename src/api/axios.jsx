69c359afcd23a8f0cd310910cb139a060a6f940b
sambatra <fitivanjanaharysambatriniaina@gmail.com>
2026-01-14 14:10:08 +0300

    urls encore

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 
+// Mettre ton backend Railway ici
 const api = axios.create({
   baseURL: "https://web-production-6a3e3.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access");
+  const token = localStorage.getItem("access"); // ton JWT
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
